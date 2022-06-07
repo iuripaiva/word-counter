c7e427e66bfdcac988c55c31eafa2b5555401401
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2022-06-07 15:49:32 -0300

    Update script.js
    change variables from var to let

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var text = "";
+let text = "";
 
 function count() {
 
@@ -6,10 +6,10 @@ function count() {
     text = String(document.getElementById("text").value);
 
     // Delete any empty spaces in the text and converts it to array
-    var textArray = text.trim().split(" ");
+    let textArray = text.trim().split(" ");
     
     // Count words based in the length of the array after deleting the empty spaces
-    var countWords = textArray.length;
+    let countWords = textArray.length;
 
     // If the array don't have any item, zero words will be counted.
     if (textArray == 0){
